0dffad9dba6d66e69c8972500293f3c80576b402
kongbaiai2@126.com <kongbaiai2@126.com>
2022-03-01 00:54:56 +0800

    upload multi file

--- src/gin/main.go
+++ src/gin/main.go
@@ -1,33 +1,140 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
+	"github.com/didip/tollbooth"
+	"github.com/didip/tollbooth/limiter"
+	"github.com/ffhelicopter/tmm/api"
+	"github.com/ffhelicopter/tmm/handler"
 	"github.com/gin-gonic/gin"
 )
 
+func submit(c *gin.Context) {
+	name := c.DefaultQuery("name", "lily")
+	c.String(200, fmt.Sprintf("hello %s\n%s", name))
+}
+
+// 定义全局的CORS中间件
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+		c.Next()
+	}
+}
+func LimitHandler(lmt *limiter.Limiter) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		httpError := tollbooth.LimitByRequest(lmt, c.Writer, c.Request)
+		if httpError != nil {
+			c.Data(httpError.StatusCode, lmt.GetMessageContentType(), []byte(httpError.Message))
+			c.Abort()
+		} else {
+			c.Next()
+		}
+	}
+}
+
+func upload(c *gin.Context) {
+
+	form, err := c.MultipartForm()
+	if err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("get err %s", err.Error()))
+	}
+	// 获取所有图片
+	files := form.File["files"]
+	// 遍历所有图片
+	for _, file := range files {
+		// 逐个存
+		// if err := c.SaveUploadedFile(file, file.Filename); err != nil {
+		dst := fmt.Sprintf("./website/tmp/%s", file.Filename)
+		if err := c.SaveUploadedFile(file, dst); err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("upload err %s", err.Error()))
+			return
+		}
+	}
+	c.String(200, fmt.Sprintf("upload ok %d files", len(files)))
+
+}
+
+// var router *gin.Engine
+
 func main() {
-
-	// Engin
-
 	router := gin.Default()
+	// 静态资源加载，本例为css,js以及资源图片
+	router.StaticFS("/public", http.Dir("./website/static"))
+	router.StaticFile("/favicon.jpg", "./website/favicon.jpg")
+	router.StaticFile("/uploads", "./website/tpl/upload.html")
 
-	//router := gin.New()
+	// 导入所有模板，多级目录结构需要这样写,tpl动态目录
+	router.LoadHTMLGlob("./website/tpl/*")
 
-	router.GET("/hello", func(context *gin.Context) {
+	// 使用全局CORS中间件。
+	// router.Use(Cors())
+	// 即使是全局中间件，在use前的代码不受影响
+	//rate-limit 限流中间件
+	lmt := tollbooth.NewLimiter(1, nil)
+	lmt.SetMessage("服务繁忙，请稍后再试...")
 
-		log.Println(">>>> hello gin start <<<<")
+	// website分组
+	v := router.Group("/")
+	{
+		v.GET("/index.html", LimitHandler(lmt), handler.IndexHandler)
+		v.GET("/add.html", handler.AddHandler)
+		v.POST("/postme.html", handler.PostmeHandler)
+		v.POST("/submit", submit)
+		v.POST("/upload", upload)
+	}
 
-		context.JSON(200, gin.H{
+	v1 := router.Group("/v1")
+	{
+		// 下面是群组中间的用法
+		// v1.Use(Cors())
+		// 单个中间件的用法
+		// v1.GET("/user/:id/*action",Cors(), api.GetUser)
+		// rate-limit
+		v1.GET("/user/:id/*action", LimitHandler(lmt), api.GetUser)
+		//v1.GET("/user/:id/*action", Cors(), api.GetUser)
+		// AJAX OPTIONS ，下面是有关OPTIONS用法的示例
+		// v1.OPTIONS("/users", OptionsUser)      // POST
+		// v1.OPTIONS("/users/:id", OptionsUser)  // PUT, DELETE
+	}
 
-			"code": 200,
+	// router.Run(":80")
+	// 这样写就可以了，下面所有代码（go1.8+）是为了优雅处理重启等动作。
+	srv := &http.Server{
+		Addr:         ":80",
+		Handler:      router,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+	go func() {
+		// 监听请求
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
 
-			"success": true,
-		})
+	// 优雅Shutdown（或重启）服务
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt) // syscall.SIGKILL
+	<-quit
+	log.Println("Shutdown Server ...")
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	select {
+	case <-ctx.Done():
+	}
+	log.Println("Server exiting")
 
-	})
-
-	// 指定地址和端口号
-
-	router.Run("localhost:9090")
+	// Listen and serve on 0.0.0.0:8000
+	// router.Run(":8000")
 }
