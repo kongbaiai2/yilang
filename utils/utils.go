d183893318b807dfacd295f4e51b499ab0911c5b
kongbaiai2 <kongbaiai2@126.com>
2023-08-05 12:27:28 +0800

    licence

--- utils/utils.go
+++ utils/utils.go
@@ -21,17 +21,18 @@ func Licence(licence string, key []byte) error {
 	}
 	strSpl := strings.Split(decrypt, "-")
 	if strSpl[0] != str {
-		return fmt.Errorf("licence is error")
+		return fmt.Errorf("licence is error: %s", licence)
 	}
 	// licen_time, err := time.ParseInLocation("2006-01-02 15:04:05", time_str, time.Local)
 	licen_int, err := strconv.ParseInt(strSpl[1], 10, 64)
 	if err != nil {
-		return fmt.Errorf("licence format error")
+		return fmt.Errorf("licence format error: %s", licence)
 	}
+
+	endLicen := licen_int + 60*60*24*300
 	// 300 day
-	if t.Unix()-licen_int > 60*60*24*300 {
-		err_new := "licence expiration: " + licence
-		return fmt.Errorf(err_new)
+	if t.Unix() > endLicen || t.Unix() < licen_int {
+		return fmt.Errorf("licence expiration: %v", time.Unix(endLicen, 0).Format(time.RFC3339))
 	}
 	return nil
 }
